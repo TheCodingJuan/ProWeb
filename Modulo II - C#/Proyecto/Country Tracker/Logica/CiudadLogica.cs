e1e3a5c25910c6c798816c5fa1f1cfa1071791b0
Juan Camacho <53156142+TheCodingJuan@users.noreply.github.com>
2020-05-21 03:13:57 -0500

    Add files via upload

---
+++ Modulo II - C#/Proyecto/Country Tracker/Logica/CiudadLogica.cs
@@ -0,0 +1,70 @@
+﻿using Modelos;
+using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class CiudadLogica
+    {
+        private CiudadRepositorio repositorio = new CiudadRepositorio();
+        public List<CiudadModel> GetListadoCiudades()
+        {
+            try 
+            {
+                return repositorio.GetListadoCiudades();
+            }
+            catch (Exception e)
+            {
+                return new List<CiudadModel>();
+            }
+        }
+        public CiudadModel GetCiudad(int id)
+        {
+            try
+            {
+                return repositorio.GetCiudad(id);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        public void CreateCiudad(CiudadModel ciudad)
+        {
+            try
+            {
+                repositorio.CreateCiudad(ciudad);
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        }
+        public void UpdateCiudad(int id, CiudadModel ciudad)
+        {
+            try
+            {
+                repositorio.UpdateCiudad(id, ciudad);
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        }
+        public void DeleteCiudad(int id)
+        {
+            try
+            {
+                repositorio.DeleteCiudad(id);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Modulo II - C#/Proyecto/Country Tracker/Logica/DepartamentoLogica.cs
@@ -0,0 +1,70 @@
+﻿using Modelos;
+using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class DepartamentoLogica
+    {
+        private DepartamentoRepositorio repositorio = new DepartamentoRepositorio();
+        public List<DepartamentoModel> GetListadoDepartamentos()
+        {
+            try
+            {
+                return repositorio.GetListadoDepartamentos();
+            }
+            catch (Exception e)
+            {
+                return new List<DepartamentoModel>();
+            }
+        }
+        public DepartamentoModel GetDepartamento(int id)
+        {
+            try
+            {
+                return repositorio.GetDepartamento(id);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        public void CreateDepartamento(DepartamentoModel departamento)
+        {
+            try
+            {
+                repositorio.CreateDepartamento(departamento);
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        }
+        public void UpdateDepartamento(int id, DepartamentoModel departamento)
+        {
+            try
+            {
+                repositorio.UpdateDepartamento(id, departamento);
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        }
+        public void DeleteDepartamento(int id)
+        {
+            try
+            {
+                repositorio.DeleteDepartamento(id);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+}
