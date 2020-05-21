3dec92b880d86225d0028f7dcde163b91d9b9e5e
Juan Camacho <53156142+TheCodingJuan@users.noreply.github.com>
2020-05-21 03:24:57 -0500

    Add files via upload

---
+++ Modulo II - C#/Proyecto/Country Tracker/ProyectoNET_20274814/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoNET_20274814.MvcApplication" Language="C#" %>
---
+++ Modulo II - C#/Proyecto/Country Tracker/ProyectoNET_20274814/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoNET_20274814.Startup))]
+namespace ProyectoNET_20274814
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
