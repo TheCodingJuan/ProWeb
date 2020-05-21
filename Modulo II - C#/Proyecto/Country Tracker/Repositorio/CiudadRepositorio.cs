f535fa6a81197e27e80c32b62603e2a854b15f29
Juan Camacho <53156142+TheCodingJuan@users.noreply.github.com>
2020-05-21 03:16:40 -0500

    Add files via upload

---
+++ Modulo II - C#/Proyecto/Country Tracker/Repositorio/CiudadRepositorio.cs
@@ -0,0 +1,85 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class CiudadRepositorio
+    {
+        public List<CiudadModel> GetListadoCiudades()
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                var listado = contexto.Ciudades.Select(c => new CiudadModel()
+                {
+                    Id = c.Id,
+                    NombreCiudad = c.NombreCiudad,
+                    Departamentos = new DepartamentoModel()
+                    {
+                        Id = c.Departamentos.Id,
+                        NombreDepartamento = c.Departamentos.NombreDepartamento
+                    },
+                    DepartamentoId = c.IdDepartamento ?? default(int)
+                }).ToList();
+                return listado;
+            }
+        }
+        public CiudadModel GetCiudad(int id)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                Ciudades ciudad = contexto.Ciudades.Find(id);
+                if (ciudad == null)
+                {
+                    return null;
+                }
+                return new CiudadModel()
+                {
+                    Id = ciudad.Id,
+                    NombreCiudad = ciudad.NombreCiudad,
+                    Departamentos = new DepartamentoModel()
+                    {
+                        Id = ciudad.Departamentos.Id,
+                        NombreDepartamento = ciudad.Departamentos.NombreDepartamento
+                    },
+                    DepartamentoId = ciudad.IdDepartamento ?? default(int)
+                };
+            }
+        }
+        public void CreateCiudad(CiudadModel ciudad)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                var entidad = new Ciudades()
+                {
+                    NombreCiudad = ciudad.NombreCiudad,
+                    IdDepartamento = ciudad.DepartamentoId
+                };
+                contexto.Ciudades.Add(entidad);
+                contexto.SaveChanges();
+            }
+        }
+        public void UpdateCiudad(int id, CiudadModel ciudad)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                Ciudades c = contexto.Ciudades.Find(id);
+                c.NombreCiudad = ciudad.NombreCiudad;
+                c.IdDepartamento = ciudad.DepartamentoId;
+                contexto.SaveChanges();
+            }
+        }
+        public void DeleteCiudad(int id)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                contexto.Ciudades.Remove(contexto.Ciudades.Find(id));
+                contexto.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ Modulo II - C#/Proyecto/Country Tracker/Repositorio/DepartamentoRepositorio.cs
@@ -0,0 +1,91 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class DepartamentoRepositorio
+    {
+        private CiudadRepositorio repositorioCiudad = new CiudadRepositorio();
+        public List<DepartamentoModel> GetListadoDepartamentos()
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                var listado = contexto.Departamentos.Select(d => new DepartamentoModel()
+                {
+                    Id = d.Id,
+                    NombreDepartamento = d.NombreDepartamento,
+                    PaisId = d.IdPais ?? default(int)
+                }).ToList();
+                return listado;
+            }
+        }
+        public DepartamentoModel GetDepartamento(int id)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                Departamentos depto = contexto.Departamentos.Find(id);
+                if(depto == null)
+                {
+                    return null;
+                }
+                return new DepartamentoModel()
+                {
+                    Id = depto.Id,
+                    NombreDepartamento = depto.NombreDepartamento,
+                    Ciudades = depto.Ciudades.Select(c => new CiudadModel()
+                    {
+                        Id = c.Id,
+                        NombreCiudad = c.NombreCiudad
+                    }).ToList(),
+                    PaisId = depto.IdPais ?? default(int),
+                    Paises = new PaisModel()
+                    {
+                        Id = depto.Paises.Id,
+                        NombrePais = depto.Paises.NombrePais
+                    }
+                };
+            }
+        }
+        public void CreateDepartamento(DepartamentoModel depto)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                var entidad = new Departamentos()
+                {
+                    NombreDepartamento = depto.NombreDepartamento,
+                    IdPais = depto.PaisId
+                };
+                contexto.Departamentos.Add(entidad);
+                contexto.SaveChanges();
+            }
+        }
+        public void UpdateDepartamento(int id, DepartamentoModel depto)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                Departamentos d = contexto.Departamentos.Find(id);
+                d.NombreDepartamento = depto.NombreDepartamento;
+                d.IdPais = depto.PaisId;
+                contexto.SaveChanges();
+            }
+        }
+        public void DeleteDepartamento(int id)
+        {
+            using (var contexto = new JaverianaNETEntities())
+            {
+                Departamentos depto = contexto.Departamentos.Find(id);
+                foreach(Ciudades c in depto.Ciudades)
+                {
+                    repositorioCiudad.DeleteCiudad(c.Id);
+                }
+                contexto.Departamentos.Remove(depto);
+                contexto.SaveChanges();
+            }
+        }
+
+    }
+}
